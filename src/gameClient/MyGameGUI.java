5dac71503adce5c307e299f9129e2f397b9473c1
Raphael Gozlan <raphaelior09@gmail.com>
2020-01-15 12:49:46 +0200

    GUI IMprovment

--- src/gameClient/MyGameGUI.java
+++ src/gameClient/MyGameGUI.java
@@ -46,6 +46,7 @@ public class MyGameGUI extends JFrame implements ActionListener, MouseListener, 
 
     private game_service game;
     private int level;                  //The level we are playing
+    //private double score = 0;
     private BufferedImage gameLayout;   //Buffer for the graph
 
     /**
@@ -72,6 +73,7 @@ public class MyGameGUI extends JFrame implements ActionListener, MouseListener, 
         this.setSize(width, height);
         this.setTitle("My Game");
         this.setLocationRelativeTo(null);
+        this.setFocusable(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         //ask for the level of the game
@@ -107,6 +109,101 @@ public class MyGameGUI extends JFrame implements ActionListener, MouseListener, 
         this.addMouseListener(this);
 
 
+    }
+
+    @Override
+    public void run() {
+        while (game.isRunning() ) {
+            System.out.println("Thread");
+            long dt =120;
+            moveRobots(game, graph);
+            try{
+                getRobots();
+                getFruits();
+                for (Robot r : robots){
+                    Point3D p = r.getLocation();
+                    repaint();
+
+                }
+                Thread.sleep(dt);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    /**
+     * Moves each of the robots along the edge,
+     * in case the robot is on a node the next destination (next edge) is chosen (randomly).
+     * @param game
+     * @param gg
+     * log
+     */
+    private void moveRobots(game_service game, DGraph gg) {
+        List<String> log = game.move();
+        if (log != null) {
+            long t = game.timeToEnd();
+            for (String robot_json : log) {
+                try {
+                    JSONObject line = new JSONObject(robot_json);
+                    JSONObject ttt = line.getJSONObject("Robot");
+                    int rid = ttt.getInt("id");
+                    int src = ttt.getInt("src");
+                    int dest = ttt.getInt("dest");
+
+                    if (dest == -1) {
+                                 dest = nextNode(src);
+                        game.chooseNextEdge(rid, dest);
+                        System.out.println("Turn to node: " + dest + "  time to end:" + (t / 1000));
+                        System.out.println(ttt);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * a very simple random walk implementation!
+     *
+     * @param g
+     * @param src
+     * @return
+     */
+    private int nextNode(int src) {
+        int ans = -1;
+        Collection<edge_data> ee = graph.getE(src);
+        Iterator<edge_data> itr = ee.iterator();
+
+
+        int s = ee.size();
+        int r = (int) (Math.random() * s);
+        int i = 0;
+        while (i < r) {
+            itr.next();
+            i++;
+        }
+        ans = itr.next().getDest();
+        return ans;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+            getClikedNode(e);
+    }
+
+    private int getClikedNode(MouseEvent e) {
+        int xFrame = e.getX();
+        int yFrame = e.getY();
+        Point3D clickedLocation = new Point3D(xFrame,yFrame);
+        for (node_data node : graph.getV()){
+            if(clickedLocation.equalsXY(node.getLocation()))
+                return node.getKey();
+        }
+        return -1;
     }
 
     private void addRobots() {
@@ -175,6 +272,82 @@ public class MyGameGUI extends JFrame implements ActionListener, MouseListener, 
         return null;
     }
 
+    public void paint(Graphics g){
+        g.clearRect(0,0,width,height);
+        if (gameLayout == null){
+            paintComponent();
+        }
+        Graphics2D layoutCan = (Graphics2D)g;
+        layoutCan.drawImage(gameLayout,null,0,0);
+
+        for (Fruit f : fruits)
+            g.drawImage(f.getImg(),(int)rescaleX(f.getLocation().x()) - 8 ,(int)(rescaleY(f.getLocation().y())) - 8 ,this);
+        for (Robot r : robots)
+            g.drawImage(r.getImg(),(int)rescaleX(r.getLocation().x()) - 8 ,(int)(rescaleY(r.getLocation().y())) - 8  ,this);
+    }
+
+    /**
+     * paint a representation of a graph
+     *
+     * //@param g - DGraph
+     */
+    public void paintComponent() {
+        if (graph.nodeSize() == 0)
+            return;
+
+        gameLayout = new BufferedImage(width,height,BufferedImage.TYPE_INT_ARGB);
+        //super.paint(g);
+        DGraph graph = this.graph;
+
+        Graphics2D graphics = gameLayout.createGraphics();
+
+        for (node_data p : graph.getV()) {
+            double xPixel = rescaleX(p.getLocation().x());
+            double yPixel = rescaleY(p.getLocation().y());
+
+
+            graphics.setColor(Color.BLUE);
+            graphics.fillOval((int) xPixel, (int) yPixel, 18, 18);
+            graphics.setColor(Color.black);
+            graphics.drawRect((int) xPixel, (int) yPixel, 18, 18);
+            graphics.setFont(new Font("TimesRoman", Font.BOLD, 15));
+            graphics.drawString(String.valueOf(p.getKey()), (int) xPixel - 2, (int) yPixel - 8);
+        }
+
+        for (node_data p : graph.getV()) {
+            if (graph.getE(p.getKey()) != null) {
+                for (edge_data edge : graph.getE(p.getKey())) {
+
+                    node_data src = graph.getNode(edge.getSrc());
+                    node_data dest = graph.getNode(edge.getDest());
+
+                    double srcXPixel = rescaleX(src.getLocation().x());
+                    double srcYPixel = rescaleY(src.getLocation().y());
+                    double destXPixel = rescaleX(dest.getLocation().x());
+                    double destYPixel = rescaleY(dest.getLocation().y());
+
+                    graphics.setColor(Color.RED);
+                    graphics.setStroke(new BasicStroke(2));
+
+                    Line2D line = new Line2D.Float(
+                            (int) srcXPixel + 8,
+                            (int) srcYPixel + 8,
+                            (int) destXPixel + 8,
+                            (int) destYPixel + 8);
+
+                    graphics.setStroke(new BasicStroke((float) 2.5));
+                    graphics.draw(line);
+
+
+                    graphics.setColor(Color.yellow);
+                    graphics.fillOval((int) ((srcXPixel * 10 + destXPixel * 2) / 12) + 3, (int) ((srcYPixel * 10 + destYPixel * 2) / 12) + 3 , 10, 10);
+
+                    graphics.setColor(Color.DARK_GRAY);
+                    graphics.drawString(String.valueOf(df2.format(edge.getWeight())), (int) ((srcXPixel * 2 + destXPixel) / 3) , (int) ((srcYPixel * 2 + destYPixel) / 3));
+                }
+            }
+        }
+    }
 
     /**
      * ask from play what level he wants to play
@@ -215,88 +388,7 @@ public class MyGameGUI extends JFrame implements ActionListener, MouseListener, 
 
     }
 
-    public void paint(Graphics g){
-        g.clearRect(0,0,width,height);
-        if (gameLayout == null){
-            paintComponent(g);
-        }
-        Graphics2D layoutCan = (Graphics2D)g;
-        layoutCan.drawImage(gameLayout,null,0,0);
-
-        for (Fruit f : fruits)
-            g.drawImage(f.getImg(),(int)rescaleX(f.getLocation().x()) - 16 ,(int)(rescaleY(f.getLocation().y())) - 16 ,this);
-        for (Robot r : robots)
-            g.drawImage(r.getImg(),(int)rescaleX(r.getLocation().x()) - 16 ,(int)(rescaleY(r.getLocation().y())) - 16 ,this);
-
-
-
-
-    }
-
     /**
-     * paint a representation of a graph
-     *
-     * @param g - DGraph
-     */
-    public void paintComponent(Graphics g) {
-        if (graph.nodeSize() == 0)
-            return;
-
-        gameLayout = new BufferedImage(width,height,BufferedImage.TYPE_INT_ARGB);
-        super.paint(g);
-        DGraph graph = this.graph;
-
-        Graphics2D graphics = gameLayout.createGraphics();
-
-        for (node_data p : graph.getV()) {
-            double xPixel = rescaleX(p.getLocation().x());
-            double yPixel = rescaleY(p.getLocation().y());
-
-
-            graphics.setColor(Color.BLUE);
-            graphics.fillOval((int) xPixel - 5, (int) yPixel - 7, 15, 15);
-            graphics.setColor(Color.black);
-            g.setFont(new Font("TimesRoman", Font.BOLD, 15));
-            graphics.drawString(String.valueOf(p.getKey()), (int) xPixel - 2, (int) yPixel - 8);
-        }
-
-        for (node_data p : graph.getV()) {
-            if (graph.getE(p.getKey()) != null) {
-                for (edge_data edge : graph.getE(p.getKey())) {
-
-                    node_data src = graph.getNode(edge.getSrc());
-                    node_data dest = graph.getNode(edge.getDest());
-
-                    double srcXPixel = rescaleX(src.getLocation().x());
-                    double srcYPixel = rescaleY(src.getLocation().y());
-                    double destXPixel = rescaleX(dest.getLocation().x());
-                    double destYPixel = rescaleY(dest.getLocation().y());
-
-                    graphics.setColor(Color.RED);
-                    graphics.setStroke(new BasicStroke(2));
-
-                    Line2D line = new Line2D.Float(
-                            (int) srcXPixel,
-                            (int) srcYPixel,
-                            (int) destXPixel,
-                            (int) destYPixel);
-
-                    graphics.setStroke(new BasicStroke((float) 2.5));
-                    graphics.draw(line);
-
-
-                    graphics.setColor(Color.yellow);
-                    graphics.fillOval((int) ((srcXPixel * 10 + destXPixel * 2) / 12) - 5, (int) ((srcYPixel * 10 + destYPixel * 2) / 12) - 6, 10, 10);
-
-                    graphics.setColor(Color.DARK_GRAY);
-                    graphics.drawString(String.valueOf(df2.format(edge.getWeight())), (int) ((srcXPixel * 2 + destXPixel) / 3) - 5, (int) ((srcYPixel * 2 + destYPixel) / 3) - 6);
-                }
-            }
-        }
-    }
-
-    /**
-     *
      * @param data denote some data to be scaled
      * @param r_min the minimum of the range of your data
      * @param r_max the maximum of the range of your data
@@ -307,9 +399,7 @@ public class MyGameGUI extends JFrame implements ActionListener, MouseListener, 
     private double rescale(double data, double r_min, double r_max, double t_min, double t_max) {
         return ((data - r_min) / (r_max-r_min)) * (t_max - t_min) + t_min;
     }
-
     /**
-     *
      * @param x,y - location of data
      * @return resolution of x/y with screen setting
      * using rescale method
@@ -317,94 +407,11 @@ public class MyGameGUI extends JFrame implements ActionListener, MouseListener, 
     private double rescaleX(double x) {
         return rescale(x,rangeX.get_min(),rangeX.get_max(),width*0.1,width - width*0.1);
     }
+
     private double rescaleY(double y) {
         return rescale(y,rangeY.get_min(),rangeY.get_max(),height*0.1,height - height*0.1);
 
     }
-
-
-    @Override
-    public void run() {
-        while (game.isRunning() ) {
-            System.out.println("Thread");
-            long dt =120;
-            moveRobots(game, graph);
-            try{
-                getRobots();
-                getFruits();
-                repaint();
-                Thread.sleep(dt);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-    }
-
-
-    /**
-     * Moves each of the robots along the edge,
-     * in case the robot is on a node the next destination (next edge) is chosen (randomly).
-     *
-     * @param game
-     * @param gg
-     * log
-     */
-    private static void moveRobots(game_service game, DGraph gg) {
-        List<String> log = game.move();
-        if (log != null) {
-            long t = game.timeToEnd();
-            for (String robot_json : log) {
-                try {
-                    JSONObject line = new JSONObject(robot_json);
-                    JSONObject ttt = line.getJSONObject("Robot");
-                    int rid = ttt.getInt("id");
-                    int src = ttt.getInt("src");
-                    int dest = ttt.getInt("dest");
-
-                    if (dest == -1) {
-                        dest = nextNode(gg, src);
-                        game.chooseNextEdge(rid, dest);
-                        System.out.println("Turn to node: " + dest + "  time to end:" + (t / 1000));
-                        System.out.println(ttt);
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    /**
-     * a very simple random walk implementation!
-     *
-     * @param g
-     * @param src
-     * @return
-     */
-    private static int nextNode(DGraph g, int src) {
-        int ans = -1;
-        Collection<edge_data> ee = g.getE(src);
-        Iterator<edge_data> itr = ee.iterator();
-        int s = ee.size();
-        int r = (int) (Math.random() * s);
-        int i = 0;
-        while (i < r) {
-            itr.next();
-            i++;
-        }
-        ans = itr.next().getDest();
-        return ans;
-    }
-
-
-
-
-
-
-
-
     /**
      * set the RangeX of the graph Range[minX,maxX]
      * go over all nodes and find min,max X
@@ -421,6 +428,7 @@ public class MyGameGUI extends JFrame implements ActionListener, MouseListener, 
 
         rangeX = new Range(min,max);
     }
+
     /**
      * set the RangeY of the graph Range[minY,maxY]
      * go over all nodes and find min,max Y
@@ -438,16 +446,9 @@ public class MyGameGUI extends JFrame implements ActionListener, MouseListener, 
         rangeY = new Range(min,max);
     }
 
+
     @Override
     public void actionPerformed(ActionEvent e) {
-
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-//        int scalePressX = e.getX();
-//        int scalePressY = e.getY();
-
     }
 
     @Override
@@ -469,5 +470,6 @@ public class MyGameGUI extends JFrame implements ActionListener, MouseListener, 
     public void mouseExited(MouseEvent e) {
 
     }
+
 
 }
