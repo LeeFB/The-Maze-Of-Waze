5675cac2f738c4b8e5c286e887124e716ec03d3f
nitaifingerhut <nitaifingerhut@192.168.1.105>
2020-01-15 18:35:32 +0200

    start gameLogic

--- src/gameClient/GameLogic.java
+++ src/gameClient/GameLogic.java
@@ -1,17 +1,73 @@
 package gameClient;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 import Server.Game_Server;
 import Server.game_service;
+import Server.robot;
 import gameComponent.*;
+import graph.algorithms.Graph_Algo;
 import graph.dataStructure.*;
 
 
 public class GameLogic
 {
-	game_service game;
-	static graph graph;
-	Thread t;
-	ArrayList <Robot> robots = new ArrayList <Robot>(); 
-	ArrayList <Fruit> fruits = new ArrayList <Fruit>();
+
+	private DGraph graph;
+	private Graph_Algo algo;
+
+
+	/**
+	 * 
+	 * @param gameLocig
+	 * get graph 
+	 * construct Algo_graph
+	 * list of robots - updaiting all the time
+	 * as well for a list of fruit
+	 * and as well U should have an UPDATE method
+	 */
+	public GameLogic(DGraph graph)
+	{
+		this.graph   = graph;
+		this.algo = algo;
+
+	}
+
+	public LinkedList<Integer> getStartingPoint(LinkedList<Fruit> fruits)
+	{
+		LinkedList<Integer> ans = new LinkedList<Integer>();
+		for (Fruit f: fruits)
+			ans.add(f.getEdge().getSrc());
+		return ans;
+	}
+
+	/*public int NextNode(LinkedList<Fruit> fruits, Robot r)                                                                                       
+	{
+		double min = algo.shortestPathDist(r.getDestNode(), fruits.get(0).getEdge().getSrc());
+		int index = 0;
+		int src = 0;
+		for (int i = 0; i < fruits.size(); i++)
+		{
+			double dist = algo.shortestPathDist(r.getSrcNode(), fruits.get(i).getEdge().getSrc());
+			if(dist < min)
+			{
+				index = i;
+			}
+			LinkedList<node_data> WhereToGo = (LinkedList<node_data>) algo.shortestPath(r.getSrcNode(), fruits.get(index).getEdge().getSrc());
+			Iterator<node_data> it = WhereToGo.iterator();
+			while(it.hasNext())
+			{
+
+			}
+
+
+
+			return ;
+		}
+
+	}*/
 }
