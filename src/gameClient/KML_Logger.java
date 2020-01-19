329a703e1f1af258c00be3a4f054b2dd3e863b4a
Raphael Gozlan <raphaelior09@gmail.com>
2020-01-19 15:35:14 +0200

    GUI Improve

--- src/gameClient/KML_Logger.java
+++ src/gameClient/KML_Logger.java
@@ -2,13 +2,9 @@ package gameClient;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 
 public class KML_Logger {
